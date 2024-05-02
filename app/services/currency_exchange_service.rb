3d1f808c93339f9d2602e70b29ec5881c5f57e60
Shufu Liao <liaovincent7801@gmail.com>
2024-05-02 18:11:24 +0800

    feat: CurrencyExchangeService 新增錯誤情況

--- app/services/currency_exchange_service.rb
+++ app/services/currency_exchange_service.rb
@@ -6,7 +6,22 @@ class CurrencyExchangeService
   end
 
   def convert(from, target, amount)
+    return { status: false, error: 'Invalid source currency' } unless @currencies.key?(from)
+    return { status: false, error: 'Invalid target currency' } unless @currencies[from].key?(target)
+    return { status: false, error: 'Invalid amount' } unless valid_number?(amount)
+    amount = amount.delete(',')
+
     result = amount.to_f * @currencies[from][target]
     { status: true, amount: number_with_delimiter(result.round(2)) }
+  end
+
+  private
+
+  def valid_number?(str)
+    if str =~ /^\d{1,3}(,\d{3})*(\.\d+)?$/ && Float(str.delete(','))
+      true
+    else
+      false
+    end
   end
 end
